6ba2f940c7daa4444654790467906bc8c713bc40
garethTurner <gareth.turner@ons.gov.uk>
2018-05-30 09:43:22 +0100

    setup liquibase and create entities

---
+++ src/main/java/uk/gov/ons/fwmt/staff_resource_service/entity/FieldPeriod.java
@@ -0,0 +1,30 @@
+package uk.gov.ons.fwmt.staff_resource_service.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "field_periods")
+public class FieldPeriod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PRIVATE)
+    private Long id;
+
+    @Column(nullable = false)
+    Date startDate;
+
+    @Column(nullable = false)
+    Date endDate;
+
+    @Column(nullable = false)
+    String fieldPeriod;
+}
+
