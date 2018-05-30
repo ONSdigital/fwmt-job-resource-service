56bf5cba1bfe09b2c15b740cdca71c82d3d3031a
soundar <soundarin@gmail.com>
2018-05-30 22:54:40 +0100

    DB changes updated

--- src/main/resources/db/changelog/v1/11_Rework_Gateway.sql
+++ src/main/resources/db/changelog/v1/11_Rework_Gateway.sql
@@ -1,17 +1,20 @@
 SET SCHEMA 'gateway';
 
+DROP TABLE gateway.staff CASCADE;
+DROP TABLE gateway.users CASCADE;
+DROP TABLE gateway.leavers CASCADE;
+DROP TABLE gateway.jobs CASCADE;
+
 CREATE TABLE gateway.tm_jobs (
     id         bigserial PRIMARY KEY,
-    tm_job_id  varchar(40) UNIQUE,
-    last_auth_no varchar(40)
+    tm_job_id  varchar(40) UNIQUE
 );
 
 CREATE TABLE gateway.tm_users (
     id           bigserial PRIMARY KEY,
     auth_no      varchar(40) UNIQUE,
     tm_username  varchar(40) NOT NULL,
-    active       boolean,
-    alternate_auth_no varchar(40)
+    active       boolean
 );
 
 CREATE TABLE gateway.field_periods (
